6f5d446f2dbf172bd7032815568698aed9e79583
Sufiyan Mansuri <133522557+sufiyan-mansuri@users.noreply.github.com>
2024-09-12 13:15:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-item");
+const openMenuButton = document.getElementById("open-menu-button");
+const closeMenuButton = document.getElementById("close-menu-button");
+
+openMenuButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+navbarLinks.forEach((link) => {
+  // Close menu when a link is clicked
+  link.addEventListener("click", () => openMenuButton.click());
+});
+
+// Close menu when the close button is clicked
+closeMenuButton.addEventListener("click", () => openMenuButton.click());
+
+/* Swiper JS */
+let swiper = new Swiper(".slide-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  /* Responsive breakpoints */
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1080: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// additional requirement
+var gallerySwiper = new Swiper('.gallery-slider', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 2500, // Change the delay as per your preference
+    disableOnInteraction: false, // Keeps autoplay active even if interacted
+  },
+  speed: 600, // Smooth transition speed
+});
+
